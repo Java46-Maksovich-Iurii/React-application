ea4fb0b404c6d576fcc52cee7bd35b94b82d2351
Iurii_Maksovich <gizmaksovitch@gmail.com>
2022-04-06 20:58:48 +0300

    added visual changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,15 @@ import Timer from './components/Timer';
 
 function App() {
   return (
-  <div>
+  <>
     <h1 className = 'title'>Current time</h1>
     <div className='timer-placeholder'>
-    <Timer interval={1000} city = 'Tel Aviv' gmt = {3} />
-    <Timer interval={1000} city = 'Beijing' gmt = {8}/>
-    <Timer interval={1000} city = 'New York' gmt = {-4}/>
-    <Timer interval={1000} city = 'London' gmt = {1}/>
+      <Timer interval={1000} city = 'Tel Aviv' gmt = {3} />
+      <Timer interval={1000} city = 'Beijing' gmt = {8}/>
+      <Timer interval={1000} city = 'New York' gmt = {-4}/>
+      <Timer interval={1000} city = 'London' gmt = {1}/>
     </div>
-  </div>
+  </>
   )
 }
 
