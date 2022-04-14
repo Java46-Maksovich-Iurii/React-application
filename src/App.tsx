b0dee248918db78e170472811fd0b7e8ffe400fd
Iurii_Maksovich <gizmaksovitch@gmail.com>
2022-04-14 19:06:07 +0300

    added matrix life game

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import './App.css';
-import Color from './components/Color';
-import InputData from './components/inputData';
-import Timer from './components/Timer';
-import timeZones from './config/time-zones';
-type ComponentNames = "input" | "timer" | "color";
+import Life from './components/Life';
+import lifeGameConfig from './config/lifeGameConfig.json'
 
 function App() {
-  const [timeZone, setTimeZone] = React.useState("Asia/Jerusalem");
-  const [color, setColor] = React.useState("Red");
-  const [componentName, setComponentName] = React.useState<ComponentNames>("input");
-  const mapComponents: Map<ComponentNames, React.ReactNode> = new Map ();
-  mapComponents.set("color", <Color color={color}></Color>);
-  mapComponents.set("input", <InputData timeZones={timeZones} injectTimeZone={setTimeZone}></InputData>);
-  mapComponents.set("timer", <Timer timeZone={timeZone}></Timer>);
-
+ 
   return (
     <>
-      {Array.from(mapComponents.keys()).map(k => <button onClick={() => setComponentName(k)}>{k}</button>)}
-      {mapComponents.get(componentName)}
+    <Life dimension={lifeGameConfig.dimension} ticInterval={lifeGameConfig.tic}></Life>
     </>
   )
 }
